fa71e6fb1cafc13dc98a7e46dd0fe8c0c2049de9
Irina <irenserge@mail.ru>
2022-01-28 17:41:45 +0300

    feat: decision

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let res = [];
+    if (matrix === undefined) return [];
+    matrix.map((item, index) => {
+        console.log(item, index);
+        if (index === 0 || index % 2 === 0) {
+          res.push(...item);
+        } else {
+          res.push(...item.reverse());
+        }
+    })
+    return res
 }
